2e4f13ab46bf993b70cdfd75cda53f6b16073852
Andrew <andrew.jones0@gmail.com>
2015-10-09 07:28:51 +1100

    Return 503 when service not found

--- app.lua
+++ app.lua
@@ -1,17 +1,23 @@
 local cjson = require "cjson"
 
-local subdomain = ngx.var.host
-
+-- Get subdomain from host
 for string in string.gmatch(ngx.var.host, "([^.]+)") do
     subdomain = string
     break
 end
 
+-- Call Mesos DNS for services where subdomain = service name
 resp = ngx.location.capture("/__mesos_dns/v1/services/_" .. subdomain .. "._tcp.marathon.mesos")
-
 backends = cjson.decode(resp.body)
 
+-- No backends, return 503 Service Unavailable
+if #backends < 1 then
+    ngx.exit(ngx.HTTP_SERVICE_UNAVAILABLE)
+end
+
+-- Seed the random number generator and choose a random backend
 math.randomseed(os.time())
 backend = backends[ math.random( 1, #backends ) ]
 
+-- Set Nginx target to backend
 ngx.var.target = "http://" .. backend.ip .. ':' .. backend.port
