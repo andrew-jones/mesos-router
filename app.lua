bad54ef07a9511a61654260808bb8970a519c08f
Andrew Jones <andrewj@softint.com.au>
2015-10-08 17:13:18 +1100

    Use subdomain to route to service by name

--- app.lua
+++ app.lua
@@ -1,7 +1,17 @@
 local cjson = require "cjson"
 
-resp = ngx.location.capture("/__mesos_dns/v1/services/_registry._tcp.marathon.mesos")
+local subdomain = ngx.var.host
+
+for string in string.gmatch(ngx.var.host, "([^.]+)") do
+    subdomain = string
+    break
+end
+
+resp = ngx.location.capture("/__mesos_dns/v1/services/_" .. subdomain .. "._tcp.marathon.mesos")
 
 backends = cjson.decode(resp.body)
-backend = backends[1]
-ngx.var.target = "http://" .. backend['ip'] .. ":"  .. backend['port']
+
+math.randomseed(os.time())
+backend = backends[ math.random( 1, #backends ) ]
+
+ngx.var.target = "http://" .. backend.ip .. ':' .. backend.port
